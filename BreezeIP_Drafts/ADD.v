40d39c4bde0179a9cfc72fc6b0367a104a53d7e3
miconnors812 <miconnors@ursinus.edu>
2024-03-14 18:00:33 -0400

    yippieeee the add (with carry) is workinggit push!

--- BreezeIP_Drafts/ADD.v
+++ BreezeIP_Drafts/ADD.v
@@ -1,17 +1,25 @@
 module halfAdder(input a, b, output out, c);
-    xor(a, b, out);
+    //xor(a, b, out);
+    assign out = a ^ b;
     assign c = a & b;
 endmodule
 module fullAdder(input a, input b, input cin, output wire out, output wire cout);
     wire ab;
     wire ab_c;
     wire aOb;
-    wire aObANDcin;
-    halfAdder abAdder(a, b, ab, ab_c);
-    xor(out, ab_c, cin);
-    assign aOb = a|b;
-    assign aObANDcin = aOb & cin;
-    assign cout = aObANDcin | ab_c;
+    wire aXb;
+    wire aXbANDcin;
+    //halfAdder abAdder(a, b, ab, ab_c);
+    // DONT PUT BACK THIS ONE (or any of them for that matter...) --> xor(out, ab_c, cin);
+    //assign out = cin ^ ab_c;
+    //assign aOb = a|b;
+    //assign aObANDcin = aOb & cin;
+    //assign cout = aObANDcin | ab_c;
+    assign aXb = a ^ b;
+    assign out = aXb ^ cin;
+    assign aOb = a & b;
+    assign aXbANDcin = aXb & cin;
+    assign cout = aXbANDcin | aOb;
 endmodule
 module add20(input [19:0] a, input [19:0] b, output wire [19:0] out, output wire cout);
     wire [19:0] carry;
--- BreezeIP_Drafts/addTestbench.v
+++ BreezeIP_Drafts/addTestbench.v
@@ -1,6 +1,8 @@
 `timescale 1ns / 1ns
 `include "ADD.v"
 
+
+//HALF ADDER TESTBENCH
 module halfAdder_tb;
 
 reg a;
@@ -36,6 +38,8 @@ initial begin
     b = 1;
     #10;
 
+    #40
+
     $display("test complete");
     $finish;
 
@@ -43,4 +47,122 @@ end
 
 endmodule
 
+
+
+// FULL ADDER TESTBENCH
+module fullAdder_tb;
+
+reg a;
+reg b;
+reg cin;
+wire out;
+wire cout;
+
+
+fullAdder Adder(a,b,cin,out,cout);
+
+initial begin
+
+    $dumpfile("ADD.vcd");
+    $dumpvars(0, fullAdder_tb);
+    #10;
+    //a = 20'b00000000000000000000;
+    //b = 20'b11111111111111111111;
+    a = 0;
+    b = 0;
+    cin = 0;
+    #10;
+    
+    a = 0;
+    b = 1;
+    cin = 0;
+    #10;
+
+    a = 1;
+    b = 0;
+    cin = 0;
+    #10;
+
+    a = 1;
+    b = 1;
+    cin = 0;
+    #10;
+
+    a = 0;
+    b = 0;
+    cin = 1;
+    #10;
+    
+    a = 0;
+    b = 1;
+    cin = 1;
+    #10;
+
+    a = 1;
+    b = 0;
+    cin = 1;
+    #10;
+
+    a = 1;
+    b = 1;
+    cin = 1;
+    #10;
+
+    $display("test complete");
+    $finish;
+
+end
+
+endmodule
+
+
+
+// 20-BIT FULL ADDER TESTBENCH
+module twentyBitAdd_tb;
+
+reg [19:0] a;
+reg [19:0] b;
+//reg cin;
+wire [19:0] out;
+wire cout;
+
+
+add20 Adder(a,b,out,cout);
+
+initial begin
+
+    $dumpfile("ADD.vcd");
+    $dumpvars(0, twentyBitAdd_tb);
+    #10;
+    a = 20'b00000000000000000000;
+    b = 20'b00000000000000000000;
+    //cin = 0;
+    #10;
+    
+    a = 20'b00000000000000000000;
+    b = 20'b01111111111111111111;
+    //cin = 0;
+    #10;
+
+    b = 20'b00000000000000000000;
+    a = 20'b01111111111111111111;
+    //cin = 0;
+    #10;
+
+    a = 20'b01111111111111111111;
+    b = 20'b01111111111111111111;
+    //cin = 0;
+    #10;
+
+    a = 20'b01010101010101010101;
+    b = 20'b01010101010101010101;
+    //cin = 0;
+    #50;
+
+    $display("test complete");
+    $finish;
+
+end
+
+endmodule
     
