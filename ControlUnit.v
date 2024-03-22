cb7bb7922e55a9c9cdcf95ece764077a16eaeaf6
NoraTheGuardian <bentucker102@gmail.com>
2024-03-22 13:10:08 -0400

    CU progress, putting things together in CU_tb.v

--- ControlUnit.v
+++ ControlUnit.v
@@ -1,7 +1,9 @@
 `include "./FlowControl.v"
+`include "./opPaths"
 module intrFetch(input [19:0] idx, input [31:0][19:0] mem, output [19:0]instr);
     wire [4:0]sel;
     assign sel = idx[4:0];
+    //replace this with a load from mem once complete
     MUX m(mem, sel, instr);
 endmodule
 module instrDecode(input [19:0] instr, input [31:0][19:0] regArr, 
@@ -35,3 +37,6 @@ module instrDecode(input [19:0] instr, input [31:0][19:0] regArr,
     DEMUX demuxW(wordW, addrW, regArrW);
     DECODE enableWrite(addrW, regWE);
 endmodule
+
+
+
